33b5d55026787603e1058e48ec2b7be06205910c
Nadine <S1510458036@students.fh-hagenberg.at>
2017-04-12 18:17:33 +0200

    static data file (recipes.json) and api-imitation (Recipe.py) added

---
+++ Recipe.py
@@ -0,0 +1,51 @@
+import json
+import random
+
+recipes = json.load(open("recipes.json"))
+
+# returns the one specific matching recipe (if available)
+def getRecipeByName(name):
+    name = name.upper()
+    for recipe in recipes:
+        if recipe["name"].upper() == name:
+            return recipe
+    return None
+
+# returns all the recipes that belong to that category (if available)
+def getRecipeByCategory(category):
+    category = category.upper()
+    matches = []
+    for recipe in recipes:
+        if recipe["category"].upper() == category:
+            matches.append(recipe)
+    if len(matches) != 0:
+        return matches
+    return None
+
+# returns a list of recipes that include that ingredient (if available)
+# parameter listOfRecipes: for follow-up questions like the second ingredient that should be included
+def getRecipeByIngredient(ingredient, listOfRecipes):
+    matches = []
+    ingredient = ingredient.upper()
+    for recipe in listOfRecipes:
+        ingredients = recipe["ingredients"]
+        for ingr in ingredients:
+            if ingredient in ingr.upper():
+                matches.append(recipe)
+    if len(matches) != 0:
+        return matches
+    return None
+
+
+# Test getRecipeByName
+#print(getRecipeByName("banana bread"))
+
+# Test getRecipeByCategory
+#print(getRecipeByCategory("main"))
+
+# Test getRecipeByIngredient
+#matches = getRecipeByIngredient("pasta", recipes)
+#matches = getRecipeByIngredient("salt", matches)       # in main - if it's the last ingredient from the user
+                                                        # chose a random out of the matches-list with
+                                                        # random.choice(matches) -> matches is the matches-list
+#print(random.choice(matches))
