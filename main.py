27b98b360ca9642aed91eedb6ba336c0e8257ee2
Diyana Petrova <diyanar.petrova@gmail.com>
2017-04-13 14:01:17 +0100

    smt

--- main.py
+++ main.py
@@ -30,14 +30,14 @@ def get_intro(recipe):
 
 @ask.launch
 def launch():
-    return question("Hello, are you looking for a specific recipe or do you want me to suggest you something?").\
+    return question("Hello, are you looking for a specific recipe or do you want me to suggest you something?"). \
         reprompt("Can I suggest you any recipe?")
 
 
 @ask.intent('RecommendationIntent')
 def recommendation():
     session.attributes['state'] = 'recommendation'
-    return question("What kind of dish do you want to make?").\
+    return question("What kind of dish do you want to make?"). \
         reprompt("Sorry, I didn't get what kind of dish you want to make, could you repeat?")
 
 
@@ -46,14 +46,16 @@ def get_category(category):
     session.attributes['category'] = category
     session.attributes['state'] = 'category'
     cat = "Do you have an ingredient you want to use or should I suggest a random one recipe?"
-    return question(cat).\
+    return question(cat). \
         reprompt(cat)
 
 
-"""
-    list = getRecipeByCategory(category)
-    random_id = randint(0, len(list)-1)
-    question("The name of the selected recipe is " + recipe_name + " and its ingredients are " + ingr + ". Do you want to continu")."""
+@ask.intent('RandomIntent')
+def get_random():
+    list = getRecipeByCategory(session.attributes['category'])
+    random_id = randint(0, len(list) - 1)
+    question(get_intro(list[random_id]) + "Do you want to continue with this recipe or hear another one?"). \
+        reprompt("Do you want to continue with this" + list[random_id]['name'] + " recipe or hear another one?")
 
 
 @ask.intent('StateIngrIntent', mapping={'ingredient': 'food'})
@@ -77,18 +79,6 @@ def get_recipe_by_ingrs():
     return filteredlist
 
 
-"""
-    recipe = list[random_id]
-    recipe_name = recipe["name"]
-    recipe_ingr = recipe["ingredients"]
-    ingr = ''
-    for ingredient in recipe_ingr:
-        ingr = ingr + ingredient + " "
-    question("The name of the selected recipe is " + recipe_name + " and its ingredients are " + ingr + ". Do you want to continu")."""
-
-
-# We want Alexa to ask to the user to state at most 3 ingredients that he wants to use.
-@ask.intent('StateIngrIntent', mapping={'ingredient': 'Ingredients'})
 @ask.intent('SpecificIntent')
 def specific():
     specQ = "Can you tell me the name of the recipe?"
@@ -126,61 +116,6 @@ def stop():
 @ask.intent('AMAZON.CancelIntent')
 def cancel():
     return stop()
-
-
-"""
-@ask.intent('HelloWorldIntent')
-def hello_world():
-    return statement("Where is Ham Ham?")
-
-
-@ask.intent('AMAZON.HelpIntent')
-def help():
-    help_text = "Ask me to say Hi, dude."
-    return question(help_text).reprompt(help_text)
-# If no response ask again, if second time there is yet no response, end session
-
-
-
-@ask.intent('AMAZON.StopIntent')
-def stop():
-    bye_text = "Hasta la vista"
-    return statement(bye_text)
-
-
-@ask.intent('AMAZON.CancelIntent')
-def cancel():
-    return stop()
-
-
-@ask.intent('NumIntent', mapping={'number': 'number'})
-def number_intent(number):
-    session.attributes['number'] = number
-    link = 'http://numbersapi.com/' + number
-    num_response = requests.get(link)
-    return question("The fact is " + num_response.text).reprompt("Ask me more about numbers, dude.")
-
-
-@ask.intent('WhatNumIntent')
-def what_num():
-    number = session.attributes['number']
-    return statement("You asked me about " + str(number))
-
-
-@ask.intent('ItalyIntent')
-def italy():
-    italy_text = "Why are you in Scotland when it is sunny in Italy?"
-    return statement(italy_text)
-
-
-@ask.intent('GetItalianFactIntent')
-def get_it_fact():
-    num_facts = 13  # increment this when adding a new fact template
-    fact_index = randint(0, num_facts-1)
-    fact_text = render_template('italy_fact_{}'.format(fact_index))
-    card_title = render_template('card_title')
-    return statement(fact_text).simple_card(card_title, fact_text)
-"""
 
 
 @ask.session_ended
